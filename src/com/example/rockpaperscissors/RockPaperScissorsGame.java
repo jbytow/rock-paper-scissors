c23c893315e9d35dbdebc129c335ea5f3805b50b
Jakub Bytow <jakub.bytow@gmail.com>
2024-02-11 14:06:39 +0100

    changed game logic to determine result based on enum instead of string comparison

--- src/com/example/rockpaperscissors/RockPaperScissorsGame.java
+++ src/com/example/rockpaperscissors/RockPaperScissorsGame.java
@@ -10,6 +10,8 @@ public class RockPaperScissorsGame {
     private final ProfileManager profileManager;
     private PlayerProfile activeProfile;
 
+
+
     public RockPaperScissorsGame(ProfileManager profileManager) {
         this.profileManager = profileManager;
     }
@@ -24,31 +26,45 @@ public class RockPaperScissorsGame {
         }
     }
 
+    public enum Move {
+        ROCK, PAPER, SCISSORS;
+
+        public static Move fromString(String move) {
+            return switch (move.toLowerCase()) {
+                case "rock" -> ROCK;
+                case "paper" -> PAPER;
+                case "scissors" -> SCISSORS;
+                default -> throw new IllegalArgumentException("Invalid move: " + move);
+            };
+        }
+    }
+
+    public enum GameResult {
+        WIN, LOSE, TIE
+    }
+
+
     public String playGame(String playerMoveString) {
         Move playerMove = Move.fromString(playerMoveString);
         Move computerMove = getRandomMove();
-        String result = determineWinner(playerMove, computerMove);
+        GameResult result = determineWinner(playerMove, computerMove);
 
-        // Update profile statistics
+        // Update profile statistics based on result
         if (activeProfile != null) {
-            switch (result) {
-                case "You win!" -> {
-                    profileManager.updateWins(activeProfile.getId());
-                    activeProfile.setWins(activeProfile.getWins() + 1);
-                }
-                case "You lose!" -> {
-                    profileManager.updateLosses(activeProfile.getId());
-                    activeProfile.setLosses(activeProfile.getLosses() + 1);
-                }
-                case "It's a tie!" -> {
-                    profileManager.updateTies(activeProfile.getId());
-                    activeProfile.setTies(activeProfile.getTies() + 1);
-                }
-            }
+            updateProfileStats(result);
         }
 
-        return "You chose " + playerMove + ", computer chose " + computerMove + ". " + result + "\n" +
-                "Wins: " + wins + ", Losses: " + losses + ", Ties: " + ties + "\n";
+
+        // Generate result message
+        String resultMessage = switch (result) {
+            case WIN -> "You win!";
+            case LOSE -> "You lose!";
+            case TIE -> "It's a tie!";
+        };
+
+        return String.format("You chose %s, computer chose %s. %s\nWins: %d, Losses: %d, Ties: %d\n",
+                playerMove, computerMove, resultMessage,
+                activeProfile.getWins(), activeProfile.getLosses(), activeProfile.getTies());
     }
 
     private Move getRandomMove() {
@@ -56,22 +72,36 @@ public class RockPaperScissorsGame {
         return Move.values()[random.nextInt(Move.values().length)];
     }
 
-    private String determineWinner(Move playerMove, Move computerMove) {
-        return switch (playerMove) {
-            case ROCK -> (computerMove == Move.SCISSORS) ? win() : lose();
-            case PAPER -> (computerMove == Move.ROCK) ? win() : lose();
-            case SCISSORS -> (computerMove == Move.PAPER) ? win() : lose();
+    private GameResult determineWinner(Move playerMove, Move computerMove) {
+        if (playerMove == computerMove) return GameResult.TIE;
+        switch (playerMove) {
+            case ROCK -> {
+                return (computerMove == Move.SCISSORS) ? GameResult.WIN : GameResult.LOSE;
+            }
+            case PAPER -> {
+                return (computerMove == Move.ROCK) ? GameResult.WIN : GameResult.LOSE;
+            }
+            case SCISSORS -> {
+                return (computerMove == Move.PAPER) ? GameResult.WIN : GameResult.LOSE;
+            }
             default -> throw new IllegalStateException("Unexpected value: " + playerMove);
-        };
+        }
     }
 
-    private String win() {
-        wins++;
-        return "You win!";
-    }
-
-    private String lose() {
-        losses++;
-        return "You lose!";
+    private void updateProfileStats(GameResult result) {
+        switch (result) {
+            case WIN -> {
+                activeProfile.setWins(activeProfile.getWins() + 1);
+                profileManager.updateWins(activeProfile.getId());
+            }
+            case LOSE -> {
+                activeProfile.setLosses(activeProfile.getLosses() + 1);
+                profileManager.updateLosses(activeProfile.getId());
+            }
+            case TIE -> {
+                activeProfile.setTies(activeProfile.getTies() + 1);
+                profileManager.updateTies(activeProfile.getId());
+            }
+        }
     }
 }
